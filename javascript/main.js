e963f84db910adfc6a9bf448dad08891ecf8438b
salahlala <salahlala303@gmail.com>
2022-05-05 00:16:51 +0200

    Updated

--- javascript/main.js
+++ javascript/main.js
@@ -97,19 +97,3 @@ scrollRight.forEach((right) => {
     });
   });
 });
-
-// Load GTM for everyone except PageSpeed Insight Tool 😉
-if (navigator.userAgent.indexOf("Chrome-Lighthouse") == -1) {
-  // Google Tag Manager as Payload for all 3rd-party JS
-  (function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-    var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != "dataLayer" ? "&l=" + l : "";
-    j.async = true;
-    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-    f.parentNode.insertBefore(j, f);
-  })(window, document, "script", "dataLayer", "GTM-XXXXXXXXXX");
-  // please replace GTM-XXXXXXXXXX with own GTM ID
-}
